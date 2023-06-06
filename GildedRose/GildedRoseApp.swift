f6dc7e19e7ebdae490ae660641d0a3293006a885
Milan Anusjak <milan.anusjak@icloud.com>
2023-06-06 21:04:13 +0200

    Initial Commit

---
+++ GildedRose/GildedRoseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GildedRoseApp.swift
+//  GildedRose
+//
+//  Created by Work on 06/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct GildedRoseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
